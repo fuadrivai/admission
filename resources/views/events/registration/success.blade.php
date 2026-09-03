68ccfe88181300dc7dbae3c2ea5fec73f63f7131
fuadrivai <fuad.strummer@gmail.com>
2026-09-03 08:05:05 +0700

    cleaning

--- resources/views/events/registration/success.blade.php
+++ resources/views/events/registration/success.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Registration Successful</title>
+    <title>Mutiara Harapan Islamic School</title>
     <link rel="stylesheet" href="/assets/compiled/css/app.css">
     <link rel="stylesheet" href="/assets/compiled/css/iconly.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
